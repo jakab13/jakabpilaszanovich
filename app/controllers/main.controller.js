9995232cbda8759fd22910419f9e3bb49ca35403
Jakab Pilaszanovich <jakab@cronnection.com>
2016-09-01 12:46:16 +0200

    Responsive and menu interaction

--- app/controllers/main.controller.js
+++ app/controllers/main.controller.js
@@ -2,9 +2,9 @@
 
 module.exports = mainController;
 
-mainController.$inject =  ['$rootScope', '$stateParams', '$location', 'MainSrv'];
+mainController.$inject = ['$rootScope', '$scope', '$stateParams', '$location', '$timeout', '$mdSidenav', 'MainSrv'];
 
-function mainController($rootScope, $stateParams, $location, MainSrv) {
+function mainController($rootScope, $scope, $stateParams, $location, $timeout, $mdSidenav, MainSrv) {
     var self = this;
 
     self.go = function(url) {
@@ -17,6 +17,7 @@ function mainController($rootScope, $stateParams, $location, MainSrv) {
     };
 
     self.filter = function(url) {
+        $mdSidenav('left').toggle();
         var path = '/' + url;
         $location.path(path);
     };
@@ -32,4 +33,24 @@ function mainController($rootScope, $stateParams, $location, MainSrv) {
         d.image = "/img/projects/" + d.name + ".jpg";
     });
 
+    self.toggleLeft = buildDelayedToggler('left');
+
+    function debounce(func, wait, context) {
+        var timer;
+        return function debounced() {
+            var context = $scope,
+                args = Array.prototype.slice.call(arguments);
+            $timeout.cancel(timer);
+            timer = $timeout(function() {
+                timer = undefined;
+                func.apply(context, args);
+            }, wait || 10);
+        };
+    }
+
+    function buildDelayedToggler(navID) {
+        return debounce(function() {
+            $mdSidenav(navID).toggle();
+        }, 200);
+    }
 }
--- app/controllers/project.controller.js
+++ app/controllers/project.controller.js
@@ -10,4 +10,15 @@ function projectController($stateParams, MainSrv) {
 
     self.projectUrl = '/views/projects/' + projectName + '.html';
 
+    self.projectData = getProject(projectName);
+
+    function getProject(projectName) {
+        var l = MainSrv.projects.length;
+        for (var i = 0; i < l; i++) {
+            if (MainSrv.projects[i].name === projectName) {
+                return MainSrv.projects[i];
+            }
+        }
+    }
+
 }
